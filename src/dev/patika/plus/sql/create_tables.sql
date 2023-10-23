5438f45eebc4522c6d39ec98b1b73f343d5e2cdb
Gökdeniz Özkan <gokdenizzkn@outlook.com>
2023-10-23 16:09:11 +0300

    Table creation sql v0.0.1

---
+++ src/dev/patika/plus/sql/create_tables.sql
@@ -0,0 +1,40 @@
+create table hotel
+(
+    id              int auto_increment,
+    name            varchar(255) not null,
+    province        varchar(55)  null,
+    state           varchar(55)  null,
+    address         text         not null,
+    email           varchar(255) not null,
+    phone_number    varchar(100) not null,
+    stars           int          null,
+    facilities      varchar(255) null,
+    accommodations  varchar(255) null,
+    all_rooms       varchar(255) null,
+    available_rooms varchar(255) null,
+    constraint hotel_pk
+        primary key (id)
+);
+
+create table room
+(
+    id         int auto_increment
+        primary key,
+    behaviourType       varchar(255) null,
+    beds       int          null,
+    size       int          null,
+    facilities varchar(255) null
+);
+
+create table season
+(
+    hotel_id                   int          not null,
+    start                      timestamp    null,
+    end                        timestamp    null,
+    adult_accommodation_prices varchar(255) null,
+    child_accommodation_prices varchar(255) null,
+    constraint season_pk
+        primary key (hotel_id),
+    constraint season_hotel_id_fk
+        foreign key (hotel_id) references hotel (id)
+);
