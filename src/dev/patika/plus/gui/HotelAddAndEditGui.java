6efaadb1511b24bb8c0e3855f8fe31241bcd7278
Gökdeniz Özkan <gokdenizzkn@outlook.com>
2023-10-24 13:18:24 +0300

    updated HotelEditGui - Basic Info panel v0.0.5

--- src/dev/patika/plus/gui/HotelAddAndEditGui.java
+++ src/dev/patika/plus/gui/HotelAddAndEditGui.java
@@ -4,6 +4,25 @@ import javax.swing.*;
 
 public class HotelAddAndEditGui {
     private JPanel wrapper;
+    private JTextField nameTextField;
+    private JLabel nameLabel;
+    private JLabel provinceLabel;
+    private JTextField provinceTextField;
+    private JLabel stateLabel;
+    private JTextField stateTextField;
+    private JLabel addressLabel;
+    private JTextField addressTextField;
+    private JLabel emailLabel;
+    private JTextField emailTextField;
+    private JLabel phoneNumberLabel;
+    private JTextField phoneNumberTextField;
+    private JSlider starsSlider;
+    private JLabel starsLabel;
+    private JPanel basicInfoPanel;
+    private JLabel basicInfoLabel;
+    private JPanel currentDetailsPanel;
+    private JLabel currentDetailsLabel;
+    private JPanel facilitiesPanel;
 
     public HotelAddAndEditGui() {
         // TODO add hotel
